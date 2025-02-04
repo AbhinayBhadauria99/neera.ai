eac5dc1ffc785e604e1190413486052e8f27e8b3
Abhinay <knowledgepoint887@gmail.com>
2025-02-05 00:38:18 +0530

    initial frontend push

---
+++ client/src/components/Section.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Section() {
+  return (
+    <div className="section-container">
+      <h1 className="heading">
+        <span>WE'RE</span> <br /> COMING <br /> SOON
+      </h1>
+      <p className="paragraph-text">
+      I’m Neera, your AI guide for smarter water conservation.  Every drop counts, and I’m here to help you save water, reduce waste, and make a difference!
+      </p>
+    </div>
+  );
+}
+
+export default Section;
